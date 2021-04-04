e52d8d0336b1b1a590efeeb973e3e3df58e97373
yeonjae02 <cyj020921@gmail.com>
2021-04-05 03:09:25 +0900

    21.04.05.
    헤더 1차 마무리

--- 클론코딩 과제/js/main.js
+++ 클론코딩 과제/js/main.js
@@ -1,1 +1,16 @@
-const
+const menuopenEl = document.getElementById('btn--open')
+const menucloseEl = document.getElementById('btn--close')
+const menuEl = document.getElementById('login--list')
+
+
+menuopenEl.addEventListener("click", function() {
+    menuopenEl.style.display = 'none'
+    menucloseEl.style.display = 'block'
+    menuEl.style.display = 'block'
+})
+
+menucloseEl.addEventListener("click", function() {
+    menuopenEl.style.display = 'block'
+    menucloseEl.style.display = 'none'
+    menuEl.style.display = 'none'
+})
