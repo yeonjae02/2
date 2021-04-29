4c941f725c066372edf1605c9986141d27690888
yeonjae02 <cyj020921@gmail.com>
2021-04-29 18:36:02 +0900

    9주차 실습

---
+++ week09/js/main.js
@@ -0,0 +1,11 @@
+const textEl = document.getElementsById("demo");
+
+window.addEventListener("resize", function(){
+window.innerWidth >= 960
+    ?textEl.innerHTML = "데스크탑"
+    : window.innerWidth >= 760
+}
+
+
+
+) 
