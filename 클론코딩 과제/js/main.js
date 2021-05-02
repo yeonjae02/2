093a5ba0e08f47d552331b12cf8aabecbb9fa076
yeonjae02 <cyj020921@gmail.com>
2021-05-03 00:21:18 +0900

    21.05.03.

--- 클론코딩 과제/js/main.js
+++ 클론코딩 과제/js/main.js
@@ -1,6 +1,8 @@
+// 로그인 회원가입 관련 js 
 const menuopenEl = document.getElementById('btn--open')
 const menucloseEl = document.getElementById('btn--close')
 const menuEl = document.getElementById('login--list')
+const menucloseE2 = document.getElementsById('closebtn')
 
 
 menuopenEl.addEventListener("click", function() {
@@ -13,4 +15,10 @@ menucloseEl.addEventListener("click", function() {
     menuopenEl.style.display = 'block'
     menucloseEl.style.display = 'none'
     menuEl.style.display = 'none'
+})
+
+menucloseE2.addEventListener("click", function() {
+    menuopenEl.style.display ='none'
+    menucloseEl.style.display = 'block'
+    menuEl.style.display = 'block'
 })
