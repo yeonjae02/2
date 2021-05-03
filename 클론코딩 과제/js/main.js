e4c44767a7d6d049a6965b1fe58b68d47bbe41b0
yeonjae02 <cyj020921@gmail.com>
2021-05-04 00:34:28 +0900

    21.05.04.

--- 클론코딩 과제/js/main.js
+++ 클론코딩 과제/js/main.js
@@ -5,6 +5,7 @@ const menucloseEl2 = document.getElementById('btn--close2')
 const menuEl = document.getElementById('login--list')
 
 
+
 menuopenEl.addEventListener("click", function() {
     menuopenEl.style.display = 'none'
     menucloseEl.style.display = 'block'
@@ -22,3 +23,42 @@ menucloseEl2.addEventListener("click", function() {
     menucloseEl.style.display = 'none'
     menuEl.style.display = 'none'
 })
+
+// 검색창 클릭 시 나타나는 검색목록들
+const searchallcontents = document.getElementById('searchAll')
+const searchmenuEl = document.getElementById('searchContents')
+const search = document.getElementById('search_box')
+const closepopular = document.getElementById('closeSearchWordList')      
+
+
+search.addEventListener("click", function() {
+    searchmenuEl.style.display ='block'
+})
+
+closepopular.addEventListener("click", function(){
+    searchmenuEl.style.display = 'none'
+})
+
+// 검색창 내 내부 요소들
+const popularbtn = document.getElementById('popular')
+const recentbtn = document.getElementById('recent')
+const popularcontents = document.getElementById('popularList')
+const recentcontents = document.getElementById('recentList')
+const remove = document.getElementById('removeAllSearchWord')
+
+popularbtn.addEventListener("click", function(){
+    popularcontents.style.display= 'block'
+    recentcontents.style.display= 'none'
+    searchmenuEl.style.height= "357px"
+    closepopular.style.top= '320px'
+})
+
+recentbtn.addEventListener("click", function(){
+    popularcontents.style.display= 'none'
+    recentcontents.style.display= 'block'
+    searchmenuEl.style.height= '200px'
+    closepopular.style.top= '170px'
+    remove.style.top= '170px'
+})
+
+
