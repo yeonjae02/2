cedb0b6b22b6ff339953d355dbf490e7f0c295c5
yeonjae02 <cyj020921@gmail.com>
2021-05-22 14:27:37 +0900

    21.05.22

--- 클론코딩 과제/js/main.js
+++ 클론코딩 과제/js/main.js
@@ -16,51 +16,53 @@
 // })
 
 
-// 로그인 회원가입 관련 js 
+// 로그인 회원가입창 아이콘 클릭 시 작은 화면이 나올 수 있게 함
 const menuopenEl = document.getElementById('btn--open')
 const menucloseEl = document.getElementById('btn--close')
 const menucloseEl2 = document.getElementById('btn--close2')
 const menuEl = document.getElementById('login--list')
 
+// 메뉴열기 버튼을 클릭했을 때 발생하는 일
 menuopenEl.addEventListener("click", function() {
     menuopenEl.style.display = 'none'
     menucloseEl.style.display = 'block'
     menuEl.style.display = 'block'
 })
+// 메뉴닫기1 버튼을 클릭했을 때 발생하는 일
 menucloseEl.addEventListener("click", function() {
     menuopenEl.style.display = 'block'
     menucloseEl.style.display = 'none'
     menuEl.style.display = 'none'
 })
-
+// 메뉴닫기2 버튼을 클릭했을 때 발생하는 일
 menucloseEl2.addEventListener("click", function() {
     menuopenEl.style.display = 'block'
     menucloseEl.style.display = 'none'
     menuEl.style.display = 'none'
 })
 
-// 검색창 클릭 시 나타나는 검색목록들
-// const search_contents = document.getElementById('searchAll') 
+// 검색창 클릭 시 검색목록이 나타날 수 있도록 함
 const searchmenuEl = document.getElementById('searchContents')
 const search = document.getElementById('search_box')
 const closepopular = document.getElementById('closeSearchWordList') 
 const searchmenu = document.getElementById('search-info')     
 
-
+// 검색창을 클릭했을 때 발생하는 일
 search.addEventListener("click", function() {
     searchmenuEl.style.display ='block'
 })
+// 검색창 닫기 버튼을 클릭했을 때 발생하는 일
 closepopular.addEventListener("click", function(){
     searchmenuEl.style.display = 'none'
 })
-// 검색창 내 내부 요소들
+// 검색창 내 내부 요소들을 각각 클릭했을 때 각각의 기능이 구현되게 함
 const popularbtn = document.getElementById('popular')
 const recentbtn = document.getElementById('recent')
 const popularcontents = document.getElementById('popularList')
 const recentcontents = document.getElementById('recentList')
 const remove = document.getElementById('removeAllSearchWord')
 
-// 인기검색어 버튼 클릭 시 발생하는 효과
+// 인기검색어 버튼 클릭 시 발생하는 일
 popularbtn.addEventListener("click", function(){
     popularcontents.style.display= 'block'
     recentcontents.style.display= 'none'
@@ -71,7 +73,7 @@ popularbtn.addEventListener("click", function(){
     recentbtn.style.backgroundColor = '#f2f2f2'
     recentbtn.style.borderBottom = '1px solid #d2d2d2'
 })
-// 최근검색어 버튼 클릭 시 발생하는 효과
+// 최근검색어 버튼 클릭 시 발생하는 일
 recentbtn.addEventListener("click", function(){
     popularcontents.style.display= 'none'
     recentcontents.style.display= 'block'
@@ -84,7 +86,9 @@ recentbtn.addEventListener("click", function(){
     popularbtn.style.borderBottom = '1px solid #d2d2d2'
 })
 
-// 헤더 아래 종합음악 전시창 js
+// 헤더 아래 종합음악 전시창에서 버튼을 눌렀을 때 변화가 나타나게 함.
+
+// 1,2,3,4,5 숫자
 const number1 = document.getElementById('btnresult1')
 const number2 = document.getElementById('btnresult2')
 const number3 = document.getElementById('btnresult3')
@@ -515,19 +519,51 @@ const scrollToTop = () => {
 // scroll to top 버튼
 const srcollToTopBtn = document.querySelector('.btn-scrollToTop')
 
+
+// 배너의 슬라이더 구현하기
+const $banners = document.querySelectorAll(".banner")
+const $dot = document.querySelectorAll(".dot")
+let current = 0;
+
+window.addEventListener("load", function() {
+    setInterval(autoSlide1, 3000);
+});
+
+const reset1 = () => {
+    $banners.forEach((banner) => {
+        banner.style.display = "none";
+    });
+    $dot.forEach((dot) => {
+        dot.style.background = "url(//image.genie.co.kr/imageg/web/main/banner_paging.png) no-repeat 6px 3px;";
+        dot.style.width = "12px";
+      });
+};
+
+const autoSlide1 = () => {
+    reset1();
+    if (current === $banners.length-1) {
+        current = -1;
+    } 
+    current++
+    $banners[current].style.display = "block"
+    $dot[current].style.background = "url(//image.genie.co.kr/imageg/web/main/banner_paging.png) no-repeat -15px 3px;"
+};
+
+// 배너 슬라이더 내 버튼 구동
 // 에디터 추천 컨텐츠 더보기 버튼 관련 js 
 const er_more = document.getElementById('morecontents')
 const er_close = document.getElementById('closecontents')
 const line3 = document.getElementById('editorRm3')
 const line4 = document.getElementById('editorRm4')
 
+// 더보기 버튼을 클릭했을 때 발생하는 일
 er_more.addEventListener("click", function() {
     line3.style.display = 'block'
     line4.style.display = 'block'
     er_more.style.display = 'none'
     er_close.style.display = 'block'
 })
-
+// 접기 버튼을 클릭했을 때 발생하는 일
 er_close.addEventListener("click", function() {
     line3.style.display = 'none'
     line4.style.display = 'none'
@@ -544,6 +580,7 @@ const tvHotContents = document.getElementById('tvHotContents')
 const mnetbrod = document.getElementById('mnetbrod')
 const tvMnetContents = document.getElementById('tvMnetContents')
 
+// 지니tv에서 추천버튼을 클릭했을 때 발생하는 일
 genietvrecomm.addEventListener("click",function() {
     genietvrecomm.style.color = "#0096ff"
     tvmenuHOT.style.color = 'black'
@@ -552,7 +589,7 @@ genietvrecomm.addEventListener("click",function() {
     tvHotContents.style.display = 'none'
     tvMnetContents.style.display = 'none'
 })
-
+// 지니tv에서 hot 버트을 클릭했을 때 발생하는 일
 tvmenuHOT.addEventListener("click", function() {
     tvmenuHOT.style.color = '#0096ff'
     genietvrecomm.style.display = 'black'
@@ -560,8 +597,8 @@ tvmenuHOT.addEventListener("click", function() {
     tvRecommContents.style.display = 'none'
     tvHotContents.style.display = 'block'
     tvMnetContents.style.display = 'none'
-
 })
+// 지니TV에서 MNET방송 버튼을 클릭했을 때 발생하는 일
 mnetbrod.addEventListener("click", function() {
     genietvrecomm.style.color = 'black'
     tvmenuHOT.style.color = 'black'
@@ -582,6 +619,7 @@ const notice3 = document.getElementById('firstnoticecontents3')
 const back = document.getElementById('material-icons_f1')
 const next = document.getElementById('material-icons_f2')
  
+// < 버튼을 클릭했을 때 생기는 일
 backnoticebtn.addEventListener("click", function() {
     if (notice3.style.display === 'block') {
         notice1.style.display = 'none'
@@ -603,6 +641,7 @@ backnoticebtn.addEventListener("click", function() {
         next.style.color = 'black'
     }
 })
+// > 버튼을 클릭했을 때 나타나는 변화
 nextnoticebtn.addEventListener("click", function() {
     if (notice2.style.display === 'block') {
         notice1.style.display = 'none'
